06b006e8c630292eefeeb7b3118490bb060b624b
Aditya Kurnia Pratamma <100213706+Adityapooo@users.noreply.github.com>
2022-04-26 15:31:03 +0700

    Add files via upload

---
+++ Biodata.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>biodata</title>
+</head>
+<body>
+<?php
+echo"Biodata Mahasiswa
+<br>
+<br>
+<table width='700' height='200' border='0' >
+<tr>
+<td width='50'>&nbsp;</td>
+<td width='200' height='5'>NAMA LENGKAP</td>
+<td>:</td>
+<td width='500'> Aditya kurnia Pratama</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>NAMA PANGGILAN</td>
+<td>:</td>
+<td>Aditya</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> NAMA LAPANGAN</td>
+<td>:</td>
+<td>Poo</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> Tempat Tanggal Lahir</td>
+<td> :</td>
+<td>Lamongan, 10 APRIL 2002</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> STATUS</td>
+<td>:</td>
+<td>MAHASISWA</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> PENDIDIKAN</td>
+<td>:</td>
+<td>UPN VETERAN JATIM </td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> ALAMAT</td>
+<td>:</td>
+<td>Desa Karang langit</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> Tinggi/Berat</td>
+<td>:</td>
+<td>165 cm/59 kg</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> AGAMA</td>
+<td>:</td>
+<td>ISLAM</td>
+</tr>
+<tr>
+<td height='21'>&nbsp;</td>
+<td> HOBI</td>
+<td>:</td>
+<td>Olaraga</td>
+</tr>
+</table>";
+?>   
+</body>
+</html>
